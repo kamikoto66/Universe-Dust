dcf76ea7e7bf40822b2782b42efac74451651597
JeongJaehyeok <kamikoto66@gmail.com>
2018-12-01 02:56:10 +0900

    ItemSceneSetting

---
+++ Universe Dust/Assets/02.Script/ItemScene/UI/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : UI {
+
+    private Text TimerText;
+    private float CurrentTime;
+
+	// Use this for initialization
+	void Start () {
+        TimerText = Vars["Value"].GetComponent<Text>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        CurrentTime = float.Parse(System.DateTime.Now.ToString("ss"));
+
+        Debug.Log(CurrentTime);
+    }
+}
